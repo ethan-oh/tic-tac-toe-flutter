eb23d69a5ea68a6582cbfb92f3eec1b96d453c89
Ethan-OH <osm6227@gmail.com>
2024-03-15 14:20:44 +0900

    위젯 메소드로 분리

--- lib/view/game/menu_screen.dart
+++ lib/view/game/menu_screen.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:tic_tac_toe_app/%08common/button.dart';
 import 'package:tic_tac_toe_app/%08common/font_style.dart';
 import 'package:tic_tac_toe_app/controller/game_controller.dart';
-import 'package:tic_tac_toe_app/view/home_screen.dart';
+import 'package:tic_tac_toe_app/view/home/home_screen.dart';
 
 class MenuScreen extends GetView<GameController> {
   const MenuScreen({super.key});
@@ -19,50 +19,64 @@ class MenuScreen extends GetView<GameController> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 60),
-                child: GetBuilder<GameController>(
-                    builder: (controller) => Text(
-                          controller.resultMessage,
-                          style: AppStyle.resultTextStyle,
-                        )),
+              GetBuilder<GameController>(
+                builder: (controller) => resultText(controller),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SimpleButton(
-                  onPressed: () => Get.defaultDialog(
-                      title: "저장",
-                      middleText: "게임 기록을 저장하시겠습니까?",
-                      buttonColor: Colors.blue,
-                      backgroundColor: Colors.white,
-                      confirmTextColor: Colors.white,
-                      textConfirm: "확인",
-                      onConfirm: () async => controller
-                          .saveRecord()
-                          .then((value) => Get.offAll(const HomeScreen())),
-                      textCancel: "취소",
-                  ),
-                  title: '기록하기',
-                  color: Colors.transparent,
-                  elevation: 0,
-                ),
+              const SizedBox(
+                height: 60,
               ),
-              SimpleButton(
-                onPressed: () => controller.restart(),
-                title: '재경기',
-                color: Colors.transparent,
-                elevation: 0,
-              ),
-              SimpleButton(
-                onPressed: () => Get.offAll(const HomeScreen()),
-                title: 'Home',
-                color: Colors.transparent,
-                elevation: 0,
-              ),
+              saveButton(controller),
+              restartButton(controller),
+              homeButton(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  SimpleButton homeButton() {
+    return SimpleButton(
+      onPressed: () => Get.offAll(() => const HomeScreen()),
+      title: 'Home',
+      color: Colors.transparent,
+      elevation: 0,
+    );
+  }
+
+  SimpleButton restartButton(GameController controller) {
+    return SimpleButton(
+      onPressed: () => controller.restart(),
+      title: '재경기',
+      color: Colors.transparent,
+      elevation: 0,
+    );
+  }
+
+  SimpleButton saveButton(GameController controller) {
+    return SimpleButton(
+      onPressed: () => Get.defaultDialog(
+        title: "저장",
+        middleText: "게임 기록을 저장하시겠습니까?",
+        buttonColor: Colors.blue,
+        backgroundColor: Colors.white,
+        confirmTextColor: Colors.white,
+        textConfirm: "확인",
+        onConfirm: () async => controller
+            .saveRecord()
+            .then((value) => Get.offAll(() => const HomeScreen())),
+        textCancel: "취소",
+      ),
+      title: '기록하기',
+      color: Colors.transparent,
+      elevation: 0,
+    );
+  }
+
+  Text resultText(GameController controller) {
+    return Text(
+      controller.resultMessage,
+      style: AppStyle.resultTextStyle,
     );
   }
 }
--- lib/view/record/records_screen.dart
+++ lib/view/record/records_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
 import 'package:tic_tac_toe_app/%08common/font_style.dart';
 import 'package:tic_tac_toe_app/controller/records_controller.dart';
+import 'package:tic_tac_toe_app/model/record_model.dart';
 import 'package:tic_tac_toe_app/view/record/record_screen.dart';
 
 class RecordsScreen extends GetView<RecordsController> {
@@ -20,88 +21,103 @@ class RecordsScreen extends GetView<RecordsController> {
           builder: (controller) => controller.records.isEmpty
               ? const Center(
                   child: Text(
-                  '저장된 게임이 없습니다.',
-                  style: AppStyle.normalTextStyle,
-                ))
-              : ListView.builder(
-                  itemCount: controller.records.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () => Get.to(() =>
-                          RecordScreen(recordModel: controller.records[index])),
-                      child: Slidable(
-                        key: ValueKey(index), // onDismissed를 위해
-                        closeOnScroll: true,
-                        endActionPane: ActionPane(
-                          motion: const DrawerMotion(),
-                          dismissible: DismissiblePane(
-                            onDismissed: () async {
-                              // 끝까지 당기면 바로 액션함
-                              int id = controller.records[index].id!;
-                              await controller.deleteRecord(id);
-                            },
-                          ),
-                          children: [
-                            SlidableAction(
-                              borderRadius: BorderRadius.circular(10),
-                              backgroundColor: Colors.red,
-                              icon: Icons.delete,
-                              label: '삭제',
-                              onPressed: (context) async {
-                                int id = controller.records[index].id!;
-                                await controller.deleteRecord(id);
-                              },
-                            ),
-                          ],
-                        ),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: Card(
-                            color: Colors.white,
-                            child: Row(
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.all(15.w),
-                                  decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: Colors.black, width: 1)),
-                                  width: 100.w,
-                                  height: 100.w,
-                                  child: resultGameBoard(
-                                    context,
-                                    boardSize:
-                                        controller.records[index].boardSize,
-                                    recordModel: controller.records[index],
-                                    isSmall: true,
-                                  ),
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      controller.records[index].result,
-                                      style: AppStyle.normalTextStyle,
-                                    ),
-                                    Text(
-                                      '조건 : ${controller.records[index].align.toString()}칸 완성',
-                                      style: AppStyle.dateTimeTextStyle,
-                                    ),
-                                    Text(
-                                      controller.records[index].dateTime,
-                                      style: AppStyle.dateTimeTextStyle,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                    '저장된 게임이 없습니다.',
+                    style: AppStyle.normalTextStyle,
+                  ),
+                )
+              : makeRecordList(controller),
         ),
       ),
     );
   }
+
+  ListView makeRecordList(RecordsController controller) {
+    return ListView.builder(
+      itemCount: controller.records.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () => Get.to(
+            () => RecordScreen(
+              recordModel: controller.records[index],
+            ),
+            fullscreenDialog: true,
+            duration: const Duration(milliseconds: 800),
+          ),
+          child: Slidable(
+            key: ValueKey(index), // onDismissed를 위해
+            closeOnScroll: true,
+            endActionPane: ActionPane(
+              motion: const DrawerMotion(),
+              dismissible: DismissiblePane(
+                // 끝까지 당기면 바로 액션함
+                onDismissed: () async {
+                  int id = controller.records[index].id!;
+                  await controller.deleteRecord(id);
+                },
+              ),
+              children: [
+                SlidableAction(
+                  borderRadius: BorderRadius.circular(10),
+                  backgroundColor: Colors.red,
+                  icon: Icons.delete,
+                  label: '삭제',
+                  onPressed: (context) async {
+                    int id = controller.records[index].id!;
+                    await controller.deleteRecord(id);
+                  },
+                ),
+              ],
+            ),
+            child: recordCard(
+              context,
+              controller.records[index],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+Widget recordCard(context, RecordModel record) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+    child: Card(
+      color: Colors.black.withOpacity(0.01),
+      child: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.all(15.w),
+            decoration:
+                BoxDecoration(border: Border.all(color: Colors.black, width: 1)),
+            width: 100.w,
+            height: 100.w,
+            child: resultGameBoard(
+              context,
+              boardSize: record.boardSize,
+              recordModel: record,
+              isSmall: true,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                record.result,
+                style: AppStyle.normalTextStyle,
+              ),
+              Text(
+                '조건 : ${record.align.toString()}칸 완성',
+                style: AppStyle.dateTimeTextStyle,
+              ),
+              Text(
+                record.dateTime,
+                style: AppStyle.dateTimeTextStyle,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
 }
